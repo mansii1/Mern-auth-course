f3b2d01ebfd36a382f07f9099f9537f9d0ec64ce
mansii1 <mansimangukiya0@gmail.com>
2025-11-06 22:27:02 +0530

    Fixed

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { connectDB } from "./db/connectDb.js";
+import { connectDB } from "./db/connectDB.js";
 import authRoutes from "./routes/auth.route.js";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -13,10 +13,10 @@ const PORT = process.env.PORT || 5002;
 const __dirname = path.resolve();
 
 app.use(
-    cors({
-        origin: "http://localhost:5173",
-        credentials: true,
-    })
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
 );
 
 app.use(express.json());
@@ -24,13 +24,13 @@ app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "frontend/dist")));
-    app.get(/.*/, (req, res) => {
-        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-    });
+  app.use(express.static(path.join(__dirname, "frontend/dist")));
+  app.get(/.*/, (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
 }
 
 app.listen(PORT, () => {
-    connectDB();
-    console.log(`Server running on port ${PORT}`);
+  connectDB();
+  console.log(`Server running on port ${PORT}`);
 });
